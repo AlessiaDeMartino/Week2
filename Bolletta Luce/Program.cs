57c17171ebf6e5acdb3f6ffd5e923acad919aeb9
Alessia <Alessia@DESKTOP-CQNR2VF>
2021-10-12 09:07:38 +0200

    Modifica Bolletta Luce

--- Bolletta Luce/Program.cs
+++ Bolletta Luce/Program.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 
 namespace Bolletta_Luce
-{
+{  /*Realizzare un’applicazione console che consenta di eseguire il calcolo della bolletta della luce.
+    Si richiede di sviluppare un menù attraverso cui l’utente può scegliere di:
+    inserire i propri dati (nome, cognome e kwH consumati);
+    richiedere il calcolo del costo della bolletta che è costituito da una quota fissa di 40€ più il prodotto tra i kwH e 10.
+    stampare a video i valori della bolletta, inclusi nome, cognome e costo pagato.
+    Ciascuna delle operazioni descritte sopra dovrà essere implementata attraverso una opportuna routine.
+    */
     class Program
     {
         static void Main(string[] args)
@@ -36,7 +42,7 @@ namespace Bolletta_Luce
             int.TryParse(Console.ReadLine(), out kwh);
             return name_surname;
         }
-        private static void Stampa(string nome_cognome, double kilowatt)
+        private static void Stampa(string nome_cognome, int kilowatt)
         {
             Console.WriteLine($"Ciao {nome_cognome}, il calcolo del costo della tua bolletta risulta: {kilowatt}");
         }
